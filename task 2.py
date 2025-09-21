57cb6ce7feec7652f676dfbe0b59709d0d3ea80f
Laimonas Mikalauskas <laimislietuvis@gmail.com>
2025-09-21 18:46:09 +0300

    Update task 2.py

--- task 2.py
+++ task 2.py
@@ -1,13 +1,13 @@
 name = "Laimonas"
 print("name")
-print( type("name") )
+print(type("name"))
 
 age = "27"
 print("age")
-print( type("age") )
+print(type("age"))
 
 height = "181"
 print("height")
-print( type("height") )
+print(type("height"))
 
 
