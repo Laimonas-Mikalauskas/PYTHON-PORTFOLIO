4dcef7494dd0e7fad82a6542c11f84860e25fe85
Laimonas Mikalauskas <laimislietuvis@gmail.com>
2025-09-25 16:24:45 +0300

    Add files via upload

---
+++ task 3.py
@@ -0,0 +1,18 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+
+@property
+def surface(self):
+    return self._border ** 2
+r = "rectangle"
+b = "border = 30"
+b1 = "border = 10"
+
+
+
+
+
+
+
