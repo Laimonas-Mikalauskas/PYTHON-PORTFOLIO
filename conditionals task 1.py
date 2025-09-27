db9456bd1bc5ef192e0b059fd0587eb8b2a0b82d
Laimonas Mikalauskas <laimislietuvis@gmail.com>
2025-09-27 12:55:29 +0300

    Add files via upload

---
+++ conditionals task 1.py
@@ -0,0 +1,11 @@
+numb1 = 100
+numb2 = 500
+numb3 = 100
+
+print(numb1 < numb2)  # True
+print(numb1 > numb2)  # False
+print(numb1 == numb3)  # True
+print(numb1 != numb3)  # False
+
+# print(type(num1) is int)
+print("a" in "Hello World")
