b4f450c59eaccf0d9b3578aef8133bcd047e9ca9
Laimonas Mikalauskas <laimislietuvis@gmail.com>
2025-09-27 12:47:58 +0300

    Add files via upload

---
+++ several arrays.py
@@ -0,0 +1,11 @@
+score = ["10", "20", "30", "40", "50", "60.5", "70.5", "80.5", "90.5", "100.5"]
+print("10, 20, 30, 40, 50, 60.5, 70.5, 80.5, 90.5, 100.5")
+print(type("10, 20, 30, 40, 50, 60.5, 70.5, 80.5, 90.5, 100.5"))
+
+place = ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th"]
+print("1st, 2nd, 3rd, 4th, 5th, 6th, 7th, 8th, 9th, 10th")
+print(type("1st, 2nd, 3rd, 4th, 5th, 6th, 7th, 8th, 9th, 10th"))
+
+actions = ["aim", "shoot", "run", "crouch", "jump"]
+print("left/right mouse button, W+Ctrl, W+X, space")
+print(type("left/right mouse button, W+Ctrl, W+X, space"))
