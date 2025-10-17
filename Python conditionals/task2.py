3b394330b376d100f420e7c2fdf81cb5424ca046
Laimonas Mikalauskas <laimislietuvis@gmail.com>
2025-10-17 22:18:06 +0300

    Add files via upload

---
+++ Python conditionals/task2.py
@@ -0,0 +1,8 @@
+numbers_list = []
+numbers_list.append(1)
+numbers_list.append(3.2)
+numbers_list.insert(1, "hello")
+print(numbers_list)
+
+
+
---
+++ Python conditionals/task3.py
@@ -0,0 +1,6 @@
+city1 = "Liverpool"
+city2 = "Kansas"
+
+print(city1.endswith("Jus"))
+
+
