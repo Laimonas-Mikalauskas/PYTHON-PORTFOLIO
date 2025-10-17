fec19ba0c44f4239168000c2d2c02c378bbb4b7b
Laimonas Mikalauskas <laimislietuvis@gmail.com>
2025-10-17 21:10:34 +0300

    Add files via upload

---
+++ Data structures 1: lists/list.py
@@ -0,0 +1,24 @@
+list = []
+list.append(1)
+list.append(2)
+list.insert(2, "June");
+list.insert(2, "April");
+list.insert(2, "April");
+
+print(list);
+
+print(list);
+print(list);
+
+months = ['January', 'February', 'March', 'April', 'May', 'June'];
+print("months");
+print("months"[0]);
+print("months"[-1]);
+print("months"[1:4]);
+print("months"[1:4 - 2]);
+print(months[1:]);
+print(months[::-2]);
+
+
+
+
