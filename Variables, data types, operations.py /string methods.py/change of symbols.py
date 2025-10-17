2130fb8766d8eaacbf84b7668dc9db6f6c830a52
Laimonas Mikalauskas <laimislietuvis@gmail.com>
2025-10-17 21:39:53 +0300

    Change comments and fix symbol replacement logic

--- Variables, data types, operations.py /string methods.py/change of symbols.py
+++ Variables, data types, operations.py /string methods.py/change of symbols.py
@@ -1,7 +1,7 @@
-# Pakeitimas kitais simboliais
+# Change with other symbols
 from pydoc import text
 
 text_w = text.replace("l", "w")
 print(text_w)  # hewwo worwd
-text_w = text_w.replace("w", "")
-print(text_w)  # heo ord
+text_w = text_w.replace("w", "l")
+print(text_w)  # hello world
