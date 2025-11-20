430ea07235aa92e9d5b9e0655b2ff854e43f1a5b
Laimonas Mikalauskas <laimislietuvis@gmail.com>
2025-11-20 12:24:33 +0200

    Delete Math directory

--- Math/multiplication.py
+++
@@ -1,9 +0,0 @@
-var = 1000 * 4
-Output: int = 4000
-print("100 * 4 = 4000")
-print(type("4000"))
-
-var = 200 * 4
-Output: int = 800
-print("200 * 4 = 800")
-print(type("800"))
--- Math/substraction.py
+++
@@ -1,6 +0,0 @@
-var = 75 - 55
-Output: int = 20
-print("77 - 55")
-print("25")
-print(type("25"))
-
